df3d29995ed7219ac864fdc847b43b28d04a5134
Mun Byung Wuk <114369511+korea539@users.noreply.github.com>
2023-01-27 14:58:55 +0900

    Add files via upload
    [UPDATE]

---
+++ Visual_Mwook.py
@@ -0,0 +1,35 @@
+# 데이터 출처 : KOSIS 국가통계포털
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import koreanize_matplotlib
+import plotly.express as px
+from glob import glob
+
+# 코랩이나 주피터에서 수행할 시 아래 코드를 사용하여 한글화 진행
+# %config InlineBackend.figure_format = 'retina'
+
+
+pd.Series([1, -2, 3]).plot(title = "한글")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
